a48ca5e44bf2eea36290e8ebb13dc5b09ef2c798
Myshae <myshaeallen@outlook.com>
2025-01-01 12:28:52 -0600

    redoing env vars

--- todo-app-backend-final/firebase.js
+++ todo-app-backend-final/firebase.js
@@ -18,7 +18,22 @@ dotenv.config();
 // Your .env file should be added to your .gitignore to keep sensitive data secure.
 //  -> This is already done for you. But double check! You really don't want your credentials pushed to the web.
 
-const creds = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+// const creds = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+
+const firebaseConfig = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
+
 
 // Initializing Firebase Admin SDK with credentials and database URL
 admin.initializeApp({
